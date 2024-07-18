dadae48f90c40aa0a8e83e521ddd0613a931bbbc
Elnumer01 <eromario.marve@gmail.com>
2024-07-18 14:50:00 -0600

    Logout

--- app/src/main/java/com/example/app_watch/presentation/HomeScreen.kt
+++ app/src/main/java/com/example/app_watch/presentation/HomeScreen.kt
@@ -49,6 +49,7 @@ import androidx.wear.compose.material.LocalTextStyle
 import kotlinx.coroutines.launch
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.snapshots.SnapshotApplyResult
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
@@ -57,6 +58,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.app_watch.Singleton
 import com.example.app_watch.presentation.Models.ApiResponse
+import com.example.app_watch.presentation.Models.LogOut
 import okhttp3.MediaType
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody
@@ -65,15 +67,46 @@ import retrofit2.Callback
 import retrofit2.Response
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(navController: NavController) {
     println("estoy en home")
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
         Text(
             "bienvenido ${Singleton.user?.nombre}",
             color = Color.Black
         )
+        Spacer(modifier = Modifier.height(16.dp)
+        )
+        Button(onClick = { cerrar(navController) },
+        //    modifier = Modifier.width(200)//
+            ) {
+        Text(text = "LogOut",
+        color = Color.Black
+        )
+        }
     }
+}
+fun cerrar (navController: NavController) {
+    val call = RetrofitClient.api.cerrarSesion()
+    print(call)
+    call.enqueue(object:Callback<LogOut>{
+        override fun onResponse(call: Call<LogOut>, response: Response<LogOut>) {
+            if(response.isSuccessful){
+                print(response.body())
+                navController.navigate("login")
+            }
+            else{
+                print(response.body())
+            }
+        }
+        override fun onFailure(call: Call<LogOut>, t: Throwable) {
+
+        }
+
+    })
 }
